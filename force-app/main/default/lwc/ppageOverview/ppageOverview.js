a23ccba41da9d51fa21c274bca80396ede030b78
soomjeet.sahoo <soomjeet.sahoo@salesforce.com>
2023-10-07 23:10:25 +0530

    updates 7Oct

--- force-app/main/default/lwc/ppageOverview/ppageOverview.js
+++ force-app/main/default/lwc/ppageOverview/ppageOverview.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
+// import IMAGES from "@salesforce/resourceUrl/test";
+// import backgroundUrl from '@salesforce/resourceUrl/BG_Img';
+export default class PpageOverview extends LightningElement {
 
-export default class PpageOverview extends LightningElement {}
+    portfolioImage = IMAGES;
+
+    get backgroundStyle() {
+        // return `background-image:url(${backgroundUrl})`;
+    }
+
+}
